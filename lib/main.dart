13a7d12e1a8aa5aa938ebf8b22b8d59e1535d8f9
y-chiba1008 <chiba8e3@gmail.com>
2024-09-09 23:40:12 +0900

    feat:画面レイアウト

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageStateCaptor2();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -121,6 +121,69 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// チャプター2
+class _MyHomePageStateCaptor2 extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+      print("HelloWorld");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(children: [
+          Icon(Icons.create),
+          Text("初めてのタイトル"), 
+        ],)
+      ),
+      body:Column(children: [
+        const Text("HelloWorld"),
+        const Text("ハローワールド"),
+        TextButton(
+          onPressed: () => {print("ボタンクリック")},
+          child: const Text("テキストボタン")
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 24.0,
+            ),
+            Icon(
+              Icons.audiotrack,
+              color: Colors.green,
+              size: 30.0,
+            ),
+            Icon(
+              Icons.beach_access,
+              color: Colors.blue,
+              size: 36.0,
+            ),
+          ],
+        ),
+      ],),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print('押した')},
+        child: const Icon(Icons.timer), 
+      ),
+      drawer: const Drawer(child: Center(child: Text("Drawer"))),
+      endDrawer: const Drawer(child: Center(child: Text("EndDrawer"))),
     );
   }
 }
